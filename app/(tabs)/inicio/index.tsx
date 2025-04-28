9115c5d02f9f0b6e8c037380e652bbd275ff61e6
Adrianius1778 <puyolquisbert@gmail.com>
2025-04-27 22:10:09 -0400

    menu v1

--- app/(tabs)/inicio/index.tsx
+++ app/(tabs)/inicio/index.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
+import { FlatList, View, Text } from 'react-native'
+
+
+const menu = [{encabezado: 'Inicio'}, {encabezado: 'Articulos'}];
 
 const Inicio = () => {
   return (
-    <View>
-      <Text className="text-4xl text-bold m-5 text-center">Inicio</Text>
-    </View>
+    <>
+      <View>
+        <Text className="text-4xl text-bold m-5 text-center">Inicio</Text>
+      </View>
+
+      <View className="absolute top-0 right-0">
+        <View className=''>
+          <Text className="hidden m-5">Menu</Text>
+        </View>
+        <View className="bg-blue-400 flex justify-content items-center" style={{width: 250, height: 800}}>
+          <FlatList 
+            data={menu}
+            renderItem={({ item }) => <Text className="mt-20 pr-20 pl-20 pt-3 pb-3 rounded-full bg-blue-300">{item.encabezado}</Text>}
+            keyExtractor={item => item.encabezado}
+          />
+        </View>
+      </View>
+    </>
   )
 }
 
